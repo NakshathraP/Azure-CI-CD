afd3e5aa62d6473456060a2babc70ef5c693b6e5
NakshathraP <nakshathragreeneem@gmail.com>
2024-11-04 22:20:49 +0530

    Jenkins check

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/", (req,res) => {
             <!DOCTYPE html>
             <html>
                 <head>
-                    <title>Map View</title>
+                    <title>Map Views</title>
                     <meta charset="utf-8" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <!-- Get the leaflet CSS file -->
